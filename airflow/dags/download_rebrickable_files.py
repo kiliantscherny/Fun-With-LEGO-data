0bb9101cb27115a05546b184d8c0f90ba63c3f89
Kilian Tscherny <90620239+kiliantscherny@users.noreply.github.com>
2024-03-03 16:59:21 +0100

    create script to download files from rebrickable

---
+++ airflow/dags/download_rebrickable_files.py
@@ -0,0 +1,56 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+
+
+def download_files_callable(url, file_names, airflow_home_directory):
+    """
+    Download files from a webpage based on the anchor text.
+
+    Args:
+    - url (str): URL of the webpage containing the files.
+    - file_names (list of str): List of file names to be downloaded.
+    """
+    # Send a GET request to the webpage
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the webpage
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Iterate over each file name and download the corresponding file
+        for file_name in file_names:
+            # Find all anchor elements containing the given file name
+            anchor_tags = soup.find_all(
+                "a", text=re.compile(rf"{file_name}\.csv\.gz", re.IGNORECASE)
+            )
+
+            if anchor_tags:
+                # Get the link (href) behind the anchor text
+                file_link = anchor_tags[0]["href"]
+
+                # Download the file
+                file_response = requests.get(file_link)
+
+                # Check if the file download was successful (status code 200)
+                if file_response.status_code == 200:
+                    # Save the file with timestamp appended to the file name
+                    file_name_with_timestamp = f"{file_name}.csv.gz"
+                    file_path = os.path.join(
+                        airflow_home_directory, file_name_with_timestamp
+                    )
+                    print(airflow_home_directory)
+                    print(file_name_with_timestamp)
+                    print(file_path)
+                    with open(file_path, "wb") as f:
+                        f.write(file_response.content)
+                    print(f"File '{file_name_with_timestamp}' downloaded successfully.")
+                else:
+                    print(f"Failed to download the file '{file_name}'.")
+            else:
+                print(f"Anchor text '{file_name}.csv.gz' not found on the webpage.")
+    else:
+        print("Failed to retrieve webpage.")
